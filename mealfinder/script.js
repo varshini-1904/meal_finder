608318a12eadec993f6492efab350072e7f279a1
varshini-1904 <varshinipalthineni@gmail.com>
2025-08-17 23:10:03 +0530

    categories carts

--- mealfinder/script.js
+++ mealfinder/script.js
@@ -1,6 +1,6 @@
 
-function categeory(){
-    sidebar.innerHTML=`<button onclick=closecategeory()><i class="fa-solid fa-circle-xmark"></i></button>`
+function categeorylist(){
+    sidebar.innerHTML=`<button onclick=closecategeorylist()><i class="fa-solid fa-circle-xmark"></i></button>`
     fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
         .then(response=>{
             return response.json();
@@ -16,6 +16,26 @@ function categeory(){
   });
 }
 
-function closecategeory(){
+function closecategeorylist(){
     document.getElementById("sidebar").innerHTML=""
 }
+
+function categoriescarts(){
+    fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+        .then(response=>{
+            return response.json();
+        })
+        .then(data=>{
+            document.getElementById("sidebar")
+            for(keys in data.categories){
+                carts.innerHTML+=`<div>
+                <img src="${data.categories[keys].strCategoryThumb}"></img>
+                <p>${data.categories[keys].strCategory}</p>
+                </div>`
+            }
+        })
+        .catch(error => {
+            console.error("Fetch error:", error);
+  });
+}
+categoriescarts()
